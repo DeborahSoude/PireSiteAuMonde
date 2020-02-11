2946f0d34bdcce98e35a7135c5d11884a9604803
Unknown <renaud.hpavie@gmail.com>
2020-02-11 10:09:35 +0100

    fix css & function toggleForm

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ function init() {
     let form = document.getElementById('name-form');
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-
+        toggleForm();
     });
 }
 
-function hideForm() {
+function toggleForm() {
     let registerPopup = document.getElementById('register-popup');
     registerPopup.classList.toggle('hidden');
 }
