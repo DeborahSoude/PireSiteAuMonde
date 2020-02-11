cb9a859ded1d91e13f60ba2743e0c7d91e2a7887
Unknown <renaud.hpavie@gmail.com>
2020-02-11 10:21:43 +0100

    Suppresion bootstrap

--- js/script.js
+++ js/script.js
@@ -4,9 +4,12 @@ function init() {
         e.preventDefault();
         toggleForm();
     });
+    setTimeout(toggleForm, 2000);
 }
 
 function toggleForm() {
     let registerPopup = document.getElementById('register-popup');
     registerPopup.classList.toggle('hidden');
 }
+
+init();
